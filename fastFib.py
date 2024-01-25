8b719f315b15d659733be898df16857cdacc69a4
pompomJ <67637461+pompomJ@users.noreply.github.com>
2024-01-25 11:16:08 +0900

    Update and rename PythonApplication17.sln to fastFib.py
    memoを使用してフィボナッチ数の値を求める。高速。

---
+++ fastFib.py
@@ -0,0 +1,12 @@
+def fastFib(n, memo = {}):
+	if n == 0 or n == 1:
+        return 1
+    try:
+        return memo[n]
+    except:
+        result = fastFib(n - 1, memo) + fastFib(n - 2, memo)
+
+        memo[n] = result
+        return result
+
+print(fastFib(135))
