598b5525ace412ba017f2030698abd9df1bfb3b7
pompomJ <67637461+pompomJ@users.noreply.github.com>
2024-09-25 13:55:51 +0900

    Update fastFib.py
    インデント修正

--- fastFib.py
+++ fastFib.py
@@ -1,10 +1,10 @@
 def fastFib(n, memo = {}):
 	if n == 0 or n == 1:
         	return 1
-    try:
-        return memo[n]
-    except:
-        result = fastFib(n - 1, memo) + fastFib(n - 2, memo)
+        try:
+        	return memo[n]
+    	except:
+        	result = fastFib(n - 1, memo) + fastFib(n - 2, memo)
 
         memo[n] = result
         return result
