7e1297bc0555d95c52c008158f3a06e026569c4c
pompomJ <67637461+pompomJ@users.noreply.github.com>
2024-09-25 13:57:08 +0900

    Update fastFib.py

--- fastFib.py
+++ fastFib.py
@@ -1,7 +1,7 @@
 def fastFib(n, memo = {}):
 	if n == 0 or n == 1:
         	return 1
-        try:
+	try:
         	return memo[n]
     	except:
         	result = fastFib(n - 1, memo) + fastFib(n - 2, memo)
